6ce6e1425c93b59e24b18c51e86127dff39836bb
Kirill Zonov <graffzon@gmail.com>
2016-04-02 17:40:01 +0300

    humanized categories

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -1,4 +1,8 @@
 class HelpRequest < ActiveRecord::Base
   belongs_to :creator, class_name: User
   belongs_to :assistant, class_name: User
+
+  validates :title, presence: true
+
+  enum category: ["Физическая помощь", "Материальная помощь"]
 end
